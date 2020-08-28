dc42f4769c529ef9f8626c4da3f0f6c9cca62f6c
sndacq <seandacquel@gmail.com>
2020-08-28 19:49:03 +0800

    Added react-bootstrap to project Added Footer with links

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './components';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
