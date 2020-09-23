276130b2d28e4cf8044e25309440e7ccabb05e92
sndacq <seandacquel@gmail.com>
2020-09-23 10:53:46 +0800

    Added eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      1, { extensions: ['.js', '.jsx'] },
+    ],
+    'no-param-reassign': [
+      'error', { props: true, ignorePropertyModificationsFor: ['state'] }],
+  },
+};
